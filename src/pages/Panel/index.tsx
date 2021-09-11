47c70cc3d314246c7d435518d5a7a1493864d149
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-09-11 17:35:59 +0530

    Clean up project

--- src/pages/Panel/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Panel from './Panel';
-import './index.css';
-
-render(<Panel />, window.document.querySelector('#app-container'));
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -4,4 +4,4 @@ import { render } from 'react-dom';
 import Popup from './Popup';
 import './index.css';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+render(<Popup />, window.document.querySelector('#root-popup'));
