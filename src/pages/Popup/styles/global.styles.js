ca547b7ca80a616ee8563d4ed437722ce29cb3ae
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-10-02 12:55:58 +0530

    Add navigation bar styles

---
+++ src/pages/Popup/styles/global.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.white};
+`;
---
+++ src/pages/Popup/styles/theme.js
@@ -0,0 +1,15 @@
+const colors = {
+  white: '#ffffff',
+  violet: '#acb2cb',
+  violetDark: '#46455c',
+  violetLight: '#f5f6fd',
+  brown: '#cac0c5',
+  brownDark: '#a88374',
+  purple: '#565893',
+  blue: '#1b6be0',
+  alert: '#ea6666',
+  border: '#e6e7ec',
+  borderDark: '#9B9BB6',
+};
+
+export const theme = { colors };
