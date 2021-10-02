e76327a237b081bc2b3d0540d9c0625e5ad1dd07
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-10-02 14:44:08 +0530

    Shift button styles to global css

--- src/pages/Popup/styles/Overview.styles.js
+++ src/pages/Popup/styles/Overview.styles.js
@@ -23,28 +23,6 @@ export const TopContainer = styled.div`
   padding-bottom: 2rem;
 `;
 
-export const Button = styled.button`
-  background: linear-gradient(
-    88.65deg,
-    rgba(86, 88, 147, 0.58) 12.52%,
-    #acb2cb 98.15%,
-    rgba(172, 178, 203, 0.54) 114.97%
-  );
-  box-shadow: 0px 4px 24px -2px rgba(0, 0, 0, 0.2);
-  border-radius: 8px;
-  outline: none;
-  border: none;
-  padding: 1.4rem 5.5rem;
-  font-size: 1.5rem;
-  color: ${(props) => props.theme.colors.white};
-  cursor: pointer;
-
-  &:hover {
-    transition: 0.2s translateY ease;
-    transform: translateY(-1px);
-  }
-`;
-
 export const AlertText = styled.p`
   font-size: 1.2rem;
   color: ${(props) => props.theme.colors.alert};
