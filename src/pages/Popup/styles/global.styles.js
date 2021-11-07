9a331991c74bc20b5ff93becdb78166a497813b5
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-11-07 15:11:51 +0530

    Add css transition to button component

--- src/pages/Popup/styles/global.styles.js
+++ src/pages/Popup/styles/global.styles.js
@@ -13,12 +13,15 @@ export const Main = styled.main`
 `;
 
 export const Button = styled.button`
-  background: linear-gradient(
-    88.65deg,
-    rgba(86, 88, 147, 0.58) 12.52%,
-    #acb2cb 98.15%,
-    rgba(172, 178, 203, 0.54) 114.97%
+  background-image: linear-gradient(
+    45deg,
+    rgba(134, 201, 110, 0.8),
+    rgba(20, 255, 0, 0.54),
+    rgba(86, 88, 147, 0.58),
+    rgba(172, 178, 203, 0.54)
   );
+  background-position: ${(props) => (props.status ? 'left' : 'right')};
+  background-size: 300%;
   box-shadow: 0px 4px 24px -2px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
   outline: none;
@@ -27,9 +30,9 @@ export const Button = styled.button`
   font-size: 1.5rem;
   color: ${(props) => props.theme.colors.white};
   cursor: pointer;
+  transition: background-position 350ms;
 
-  &:hover {
-    transition: 0.2s translateY ease;
-    transform: translateY(-1px);
+  :hover {
+    background-position: left;
   }
 `;
